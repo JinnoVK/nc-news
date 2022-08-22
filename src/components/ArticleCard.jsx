d02040f8125639b7ba1844bf1f80b8f64a1f19e5
JinnoVK <pseudo@live.co.uk>
2022-08-22 14:35:00 +0100

    implement MUI nav with routing functionality

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -4,6 +4,7 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
+import dayjs from "dayjs";
 
 export default function ArticleCard({ article }) {
   return (
@@ -28,7 +29,7 @@ export default function ArticleCard({ article }) {
                   Posted by: {article.author}
                 </Typography>
                 <Typography variant="caption" color="text.secondary">
-                  {article.created_at}
+                  {dayjs(article.created_at).format("DD/MM/YYYY")}
                 </Typography>
               </div>
               <Typography gutterBottom variant="h5" component="div">
