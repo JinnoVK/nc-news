97b05352d868a5b5a7ba2a23f045a8bbcc8dfda5
JinnoVK <pseudo@live.co.uk>
2022-08-24 10:58:36 +0100

    implement parameter fetching functionality

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -2,23 +2,27 @@ import { useState, useEffect } from "react";
 import { getArticles } from "../api/api";
 import ArticleCard from "./ArticleCard";
 import { useParams } from "react-router-dom";
+import Sorting from "./Sorting";
 
 const ArticleList = () => {
   let { topic } = useParams();
+  let params = new URL(document.location).searchParams;
+  let sortBy = params.get("sort") || "created_at";
+  let orderType = params.get("order" || "desc") || "desc";
 
   const [isLoading, setIsLoading] = useState(true);
-
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    getArticles(topic).then(({ articles }) => {
+    getArticles(topic, orderType, sortBy).then(({ articles }) => {
       setArticles(articles);
       setIsLoading(false);
     });
-  }, [topic]);
+  }, [topic, orderType, sortBy]);
   if (isLoading) return <p>Loading...</p>;
   return (
     <div className="ArticleList">
+      <Sorting />
       {articles.map((article) => {
         return <ArticleCard key={article.article_id} article={article} />;
       })}
